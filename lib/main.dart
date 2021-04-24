d2b20d1ba2c2328b7d093b3db341622e974cf84a
Pankaj Nikam <pankaj-nikam@users.noreply.github.com>
2021-04-24 09:37:19 +0530

    Added skeleton for the project. Fixed the build errors in initial commit.

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,25 @@
 import 'package:flutter/material.dart';
 
+import 'package:provider/provider.dart';
+
+import './screens/places_list_screen.dart';
+import './providers/great_places_provider.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Great Places',
-      theme: ThemeData(
-        primarySwatch: Colors.indigo,
-        accentColor: Colors.amber,
+    return ChangeNotifierProvider.value(
+      value: GreatPlaces(),
+      child: MaterialApp(
+        title: 'Great Places',
+        theme: ThemeData(
+          primarySwatch: Colors.indigo,
+          accentColor: Colors.amber,
+        ),
+        home: PlacesListScreen(),
       ),
-      home: ...,
     );
   }
 }
---
+++ lib/models/place.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+import 'package:flutter/foundation.dart';
+import 'place_location.dart';
+
+class Place {
+  final String id;
+  final String title;
+  final PlaceLocation location;
+  final File image;
+
+  Place({
+    @required this.id,
+    @required this.title,
+    @required this.location,
+    @required this.image,
+  });
+}
