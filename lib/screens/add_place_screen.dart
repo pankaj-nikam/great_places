29889cbed925cdd95621e3246ce96c81f0c9edea
Pankaj Nikam <pankaj-nikam@users.noreply.github.com>
2021-04-26 09:52:03 +0530

    Added location package. Added widget to get location. Added location widget to add place screen.

--- lib/screens/add_place_screen.dart
+++ lib/screens/add_place_screen.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:great_places/providers/great_places_provider.dart';
 import 'package:great_places/widgets/image_input.dart';
+import 'package:great_places/widgets/location_input.dart';
 import 'package:provider/provider.dart';
 
 class AddPlaceScreen extends StatefulWidget {
@@ -52,7 +53,11 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
                     SizedBox(
                       height: 10,
                     ),
-                    ImageInput(_selectImage)
+                    ImageInput(_selectImage),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    LocationInput()
                   ],
                 ),
               ),
---
+++ lib/widgets/location_input.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class LocationInput extends StatefulWidget {
+  @override
+  _LocationInputState createState() => _LocationInputState();
+}
+
+class _LocationInputState extends State<LocationInput> {
+  String _previewImageUrl;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 170,
+          decoration: BoxDecoration(border: Border.all(width: 1)),
+          alignment: Alignment.center,
+          width: double.infinity,
+          child: _previewImageUrl == null
+              ? Text(
+                  'No Location Chosen',
+                  textAlign: TextAlign.center,
+                )
+              : Image.network(
+                  _previewImageUrl,
+                  fit: BoxFit.contain,
+                  width: double.infinity,
+                ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton.icon(
+              onPressed: () {},
+              icon: Icon(Icons.location_on),
+              label: Text('Current Location'),
+            ),
+            TextButton.icon(
+              onPressed: () {},
+              icon: Icon(Icons.map),
+              label: Text('Select on Map'),
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}
